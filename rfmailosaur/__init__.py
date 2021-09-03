4f6400c7b4771478c5eebf1842a2c7cd75d88a25
Andrea Gubellini <andreagubellini@gmail.com>
2021-09-03 16:50:43 +0200

    refactor

--- rfmailosaur/__init__.py
+++ rfmailosaur/__init__.py
@@ -1,5 +1,26 @@
-from rfmailosaur import rfmailosaur
+from rfmailosaur.keywords import keywords
+from rfmailosaur.version import VERSION
+from mailosaur import MailosaurClient
+from mailosaur.models import SearchCriteria
 
-__author__ = 'Andrea Gubellini'
-__email__ = 'agubellini@yahoo.com'
-__version__ = "0.2"
+
+class rfmailosaur(keywords):
+    __version__ = VERSION
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+
+    def __init__(self, API_KEY, server_id, server_domain) -> None:
+        """
+        The library needs a few arguments in order to work properly:
+
+        - API_KEY which you can retrieve from your mailosaur dashboard
+
+        - server_id which you can retrieve from your mailosaur dashboard
+
+        - server_domain which you can retrieve from your mailosaur dashboard
+
+        Set these arguments when importing the library in the .robot file or set a __init__.robot file with the import and parameters.
+        """
+        self.mailosaur = MailosaurClient(API_KEY)
+        self.server_id = server_id
+        self.server_domain = server_domain
+        self.criteria = SearchCriteria()
--- rfmailosaur/keywords.py
+++ rfmailosaur/keywords.py
@@ -1,29 +1,9 @@
 import re
-from mailosaur import MailosaurClient
-from mailosaur.models import SearchCriteria
 from robot.api.deco import keyword, library
 from robot.api import logger
 
 
-class rfmailosaur:
-    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-
-    def __init__(self, API_KEY, server_id, server_domain) -> None:
-        """
-        The library needs a few arguments in order to work properly:
-
-        - API_KEY which you can retrieve from your mailosaur dashboard
-
-        - server_id which you can retrieve from your mailosaur dashboard
-
-        - server_domain which you can retrieve from your mailosaur dashboard
-
-        Set these arguments when importing the library in the .robot file or set a __init__.robot file with the import and parameters.
-        """
-        self.mailosaur = MailosaurClient(API_KEY)
-        self.server_id = server_id
-        self.server_domain = server_domain
-        self.criteria = SearchCriteria()
+class keywords(object):
 
     def email_subject_should_match(self, regex: str):
         """
