41024bdddb237c870b3fa9081e039ba0546476c9
Andrea Gubellini <andreagubellini@gmail.com>
2021-09-06 15:16:10 +0200

    bump to 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="robotframework-mailosaur",
-    version="0.3",
+    version="0.4",
     author="Prima.it",
     author_email="",
     description="Mailosaur library for Robot framework",
