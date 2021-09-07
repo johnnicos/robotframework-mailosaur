34ea2d489bd2bff66ff5a10d6d2a9bc228ffb503
Andrea Gubellini <andreagubellini@gmail.com>
2021-09-07 18:09:44 +0200

    add message keywords and refactor

--- rfmailosaur/keywords.py
+++ rfmailosaur/keywords.py
@@ -5,23 +5,27 @@ from robot.api import logger
 
 class keywords(object):
 
-    def email_subject_should_match(self, regex: str):
+    def email_subject_should_match(self, regex: str, message):
         """
         Checks the email subject of the last email received on the current server_domain matches the given regular expression.
         """
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         check = bool(re.match(r'{}'.format(regex), last_email.subject))
         if check is False:
             raise Exception(
                 "The regexp does not match {}".format(last_email.subject))
 
-    def email_subject_should_contain(self, matcher: str):
+    def email_subject_should_contain(self, matcher: str, message):
         """
         Checks the email subject of the last email received on the current server_domain contains the matcher.
         """
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         try:
             assert matcher in last_email.subject
         except AssertionError as e:
@@ -38,13 +42,15 @@ class keywords(object):
         except Exception as e:
             raise e
 
-    def email_should_have_links(self, links_number: int):
+    def email_should_have_links(self, links_number: int, message):
         """
         Checks the last email contains X number of links where X == links_number.
         """
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(
             self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         links = len(last_email.html.links)
         try:
             assert links == links_number
@@ -52,13 +58,15 @@ class keywords(object):
             raise Exception("AssertionError: {0} does not equal {1}".format(
                 links, links_number))
 
-    def email_should_have_attachments(self, attachments_number: int):
+    def email_should_have_attachments(self, attachments_number: int, message):
         """
         Checks the last email contains X number of attachments where X == attachments_number.
         """
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(
             self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         attachments = len(last_email.attachments)
         try:
             assert attachments == attachments_number
@@ -66,7 +74,7 @@ class keywords(object):
             raise Exception("AssertionError: {0} does not equal {1}".format(
                 attachments, attachments_number))
 
-    def email_body_should_contain(self, matcher, case_insensitive: bool):
+    def email_body_should_contain(self, matcher, case_insensitive: bool, message):
         """
         Checks the last email's body contains a specific string (matcher).
 
@@ -75,6 +83,8 @@ class keywords(object):
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(
             self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         text = last_email.text.body
         if case_insensitive is True:
             text = text.lower()
@@ -85,23 +95,27 @@ class keywords(object):
             raise Exception("AssertionError: {0} is not contained {1}".format(
                 matcher, text))
 
-    def email_links_should_contain_text(self, text: str):
+    def email_links_should_contain_text(self, text: str, message):
         """
         Checks if atleast one of the links contained in the last email contains text.
         """
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(
             self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         links = [link.text for link in last_email.text.links]
         assert any(map(lambda link: text in link, links))
 
-    def email_sender_should_be(self, matcher: str):
+    def email_sender_should_be(self, matcher: str, message):
         """
         Checks that last email sender matches the given matcher.
         """
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(
             self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         sender = last_email.sender[0].email
         try:
             assert sender == matcher
@@ -109,13 +123,15 @@ class keywords(object):
             raise Exception("AssertionError: '{0}' does not match sender '{1}'".format(
                 matcher, sender))
 
-    def html_content_should_contain_text(self, matcher: str, case_insensitive):
+    def html_content_should_contain_text(self, matcher: str, case_insensitive, message):
         """
         Checks that last email's HTML content contains sub-string
         """
         self.criteria.sent_to = self.server_domain
         last_email = self.mailosaur.messages.get(
             self.server_id, self.criteria)
+        if len(message) > 0:
+            last_email = message
         html = last_email.html.body
         if case_insensitive is True:
             html = html.lower()
@@ -125,3 +141,20 @@ class keywords(object):
         except AssertionError as e:
             raise Exception("AssertionError: '{0}' is not contained in '{1}'".format(
                 matcher, html))
+
+    def list_emails(self):
+        """
+        Returns a list of all email messages
+        """
+        self.criteria.sent_to = self.server_domain
+        results = self.mailosaur.messages.list(self.server_id)
+        return results.items
+
+    def get_last_email(self):
+        """
+        Returns last email message
+        """
+        self.criteria.sent_to = self.server_domain
+        results = self.mailosaur.messages.list(self.server_id)
+        last_email = results.items[0]
+        return last_email
